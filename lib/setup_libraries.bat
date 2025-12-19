0c1d564e9cb5ae5c1c8bb15f0d7390bd8577b97e
tndiao74 <tndiao74@gmail.com>
2025-12-19 13:37:48 -0600

    Windows library setup.

--- lib/setup_libraries.bat
+++ lib/setup_libraries.bat
@@ -1,15 +1,9 @@
 
-:: install vcpkg
-git clone https://github.com/microsoft/vcpkg.git
+set zip="C:\Program Files\7-Zip\7z.exe"
 
-cd vcpkg
-
-:: setup for windows
-bootstrap-vcpkg.bat
-
-:: install AWS SDK libraries
-vcpkg.exe install openssl --recurse
-
-cd ..
+%zip% x .\aws\windows\windows.zip -y -o.\aws\windows
+%zip% x .\curl\windows\windows.zip -y -o.\curl\windows
+%zip% x .\ssl\windows\windows.zip -y -o.\ssl\windows
+%zip% x .\zlib\windows\windows.zip -y -o.\zlib\windows
 
 pause
